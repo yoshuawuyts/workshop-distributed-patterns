a73f06b52c216869cc204fbdcfe1e905ff72b3b1
Yoshua Wuyts <i@yoshuawuyts.com>
2016-05-10 14:47:14 +0700

    01: init

--- solutions/01/index.js
+++ solutions/01/index.js
@@ -1,7 +1,11 @@
 const http = require('http')
 const fs = require('fs')
 
-const server = http.createServer(function (req, res) {
-  res.end('{ "message": "ACK" }')
-})
-server.listen(8080)
+const LOGFILE = '/tmp/patterns-workshop.db'
+
+http.createServer(function (req, res) {
+  const ws = fs.createWriteStream(LOGFILE, { flags: 'a', encoding: 'utf8' })
+  req.pipe(ws)
+  res.statusCode = 200
+  res.end()
+}).listen(8080)
